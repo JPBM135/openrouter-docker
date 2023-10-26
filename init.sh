e2911dc561af575e16b1e5cebd9c9cf09a52165c
JPBM135 <jpedrobm0@gmail.com>
2023-10-26 14:08:41 +0000

    feat: add cert generation

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
 sudo curl https://download.geofabrik.de/south-america/brazil-231001.osm.pbf -o ./docker/data/brazil-01.osm.pbf
+sudo openssl req -x509 -nodes -newkey rsa:4096 -keyout nginx/self_signed.key -out nginx/self_signed.crt -days 9999 -subj "/C=US/ST=California/L=San Francisco/O=My Organization/OU=My Unit/CN=mydomain.com"
 docker compose up --build --detach
